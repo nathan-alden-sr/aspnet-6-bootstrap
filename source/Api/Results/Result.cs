19eeacce52c987d932865fbd1b68cdd3fcd9ec25
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2021-11-08 20:41:14 -0500

    Renamed WithFormatting to AddFormatter

--- source/Api/Results/Result.cs
+++ source/Api/Results/Result.cs
@@ -54,7 +54,7 @@ public class Result : IResult
         return this;
     }
 
-    public Result WithFormatting(Action<ActionContext> formatter)
+    public Result AddFormatter(Action<ActionContext> formatter)
     {
         ThrowIfNull(formatter, nameof(formatter));
 
@@ -74,24 +74,26 @@ public class Result : IResult
 
     // 100 Continue
 
-    public static Result Continue() => new(StatusCodes.Status100Continue);
+    public static Result Continue() =>
+        new(StatusCodes.Status100Continue);
 
     // 200 OK
 
-    public static Result Ok() => new(StatusCodes.Status200OK);
+    public static Result Ok() =>
+        new(StatusCodes.Status200OK);
 
     // 201 Created
 
     public static Result Created(string location) =>
         new Result(StatusCodes.Status201Created)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result Created(Uri location) =>
         Created(location.ToString());
 
     public static Result CreatedAtAction(string? actionName, string? controllerName, object? routeValues) =>
         new Result(StatusCodes.Status201Created)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     var request = context.HttpContext.Request;
@@ -116,7 +118,7 @@ public class Result : IResult
 
     public static Result CreatedAtRoute(string? routeName, object? routeValues) =>
         new Result(StatusCodes.Status201Created)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     ThrowIfNull(context, nameof(context));
@@ -143,14 +145,14 @@ public class Result : IResult
 
     public static Result Accepted(string? location) =>
         new Result(StatusCodes.Status202Accepted)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result Accepted(Uri? location) =>
         Accepted(location?.ToString());
 
     public static Result AcceptedAtAction(string? actionName, string? controllerName, object? routeValues) =>
         new Result(StatusCodes.Status202Accepted)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     var request = context.HttpContext.Request;
@@ -175,7 +177,7 @@ public class Result : IResult
 
     public static Result AcceptedAtRoute(string? routeName, object? routeValues) =>
         new Result(StatusCodes.Status202Accepted)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     ThrowIfNull(context, nameof(context));
@@ -207,15 +209,15 @@ public class Result : IResult
 
     public static Result MultipleChoices(string? location) =>
         new Result(StatusCodes.Status300MultipleChoices)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result MultipleChoices(Uri? location) =>
         new Result(StatusCodes.Status300MultipleChoices)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location?.ToString());
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location?.ToString());
 
     public static Result MultipleChoicesWithPreferredAction(string? actionName, string? controllerName, object? routeValues) =>
         new Result(StatusCodes.Status300MultipleChoices)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     var request = context.HttpContext.Request;
@@ -240,7 +242,7 @@ public class Result : IResult
 
     public static Result MultipleChoicesWithPreferredRoute(string? routeName, object? routeValues) =>
         new Result(StatusCodes.Status300MultipleChoices)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     ThrowIfNull(context, nameof(context));
@@ -267,7 +269,7 @@ public class Result : IResult
 
     public static Result MovedPermanently(string location) =>
         new Result(StatusCodes.Status301MovedPermanently)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result MovedPermanently(Uri location) =>
         MovedPermanently(location.ToString());
@@ -276,7 +278,7 @@ public class Result : IResult
 
     public static Result Found(string location) =>
         new Result(StatusCodes.Status302Found)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result Found(Uri location) =>
         Found(location.ToString());
@@ -285,20 +287,21 @@ public class Result : IResult
 
     public static Result SeeOther(string location) =>
         new Result(StatusCodes.Status302Found)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result SeeOther(Uri location) =>
         SeeOther(location.ToString());
 
     // 304 Not Modified
 
-    public static Result NotModified() => new(StatusCodes.Status304NotModified);
+    public static Result NotModified() =>
+        new(StatusCodes.Status304NotModified);
 
     // 307 Temporary Redirect
 
     public static Result TemporaryRedirect(string location) =>
         new Result(StatusCodes.Status307TemporaryRedirect)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result TemporaryRedirect(Uri location) =>
         TemporaryRedirect(location.ToString());
@@ -307,14 +310,15 @@ public class Result : IResult
 
     public static Result PermanentRedirect(string location) =>
         new Result(StatusCodes.Status308PermanentRedirect)
-            .WithFormatting(context => context.HttpContext.Response.Headers.Location = location);
+            .AddFormatter(context => context.HttpContext.Response.Headers.Location = location);
 
     public static Result PermanentRedirect(Uri location) =>
         PermanentRedirect(location.ToString());
 
     // 400 Bad Request
 
-    public static Result BadRequest() => new(StatusCodes.Status400BadRequest);
+    public static Result BadRequest() =>
+        new(StatusCodes.Status400BadRequest);
 
     public static StandardJsonResult<IEnumerable<ValidationFailureResultData>> BadRequest(
         string? message,
@@ -337,35 +341,39 @@ public class Result : IResult
 
     public static Result Unauthorized(StringValues wwwAuthenticate) =>
         new Result(StatusCodes.Status401Unauthorized)
-            .WithFormatting(context => context.HttpContext.Response.Headers.WWWAuthenticate = wwwAuthenticate);
+            .AddFormatter(context => context.HttpContext.Response.Headers.WWWAuthenticate = wwwAuthenticate);
 
     // 403 Forbidden
 
-    public static Result Forbidden() => new(StatusCodes.Status403Forbidden);
+    public static Result Forbidden() =>
+        new(StatusCodes.Status403Forbidden);
 
     // 404 Not Found
 
-    public static Result NotFound() => new(StatusCodes.Status404NotFound);
+    public static Result NotFound() =>
+        new(StatusCodes.Status404NotFound);
 
     // 405 Method Not Allowed
 
-    public static Result MethodNotAllowed() => new(StatusCodes.Status405MethodNotAllowed);
+    public static Result MethodNotAllowed() =>
+        new(StatusCodes.Status405MethodNotAllowed);
 
     // 406 Not Acceptable
 
-    public static Result NotAcceptable() => new(StatusCodes.Status406NotAcceptable);
+    public static Result NotAcceptable() =>
+        new(StatusCodes.Status406NotAcceptable);
 
     // 407 Proxy Authentication Required
 
     public static Result ProxyAuthenticationRequired(StringValues proxyAuthenticate) =>
         new Result(StatusCodes.Status407ProxyAuthenticationRequired)
-            .WithFormatting(context => context.HttpContext.Response.Headers.ProxyAuthenticate = proxyAuthenticate);
+            .AddFormatter(context => context.HttpContext.Response.Headers.ProxyAuthenticate = proxyAuthenticate);
 
     // 408 Request Timeout
 
     public static Result RequestTimeout(bool closeConnection = true) =>
         new Result(StatusCodes.Status408RequestTimeout)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     if (closeConnection)
@@ -376,67 +384,80 @@ public class Result : IResult
 
     // 409 Conflict
 
-    public static Result Conflict() => new(StatusCodes.Status409Conflict);
+    public static Result Conflict() =>
+        new(StatusCodes.Status409Conflict);
 
     // 410 Gone
 
-    public static Result Gone() => new(StatusCodes.Status410Gone);
+    public static Result Gone() =>
+        new(StatusCodes.Status410Gone);
 
     // 411 Length Required
 
-    public static Result LengthRequired() => new(StatusCodes.Status411LengthRequired);
+    public static Result LengthRequired() =>
+        new(StatusCodes.Status411LengthRequired);
 
     // 412 Preconditon Failed
 
-    public static Result PreconditionFailed() => new(StatusCodes.Status412PreconditionFailed);
+    public static Result PreconditionFailed() =>
+        new(StatusCodes.Status412PreconditionFailed);
 
     // 413 Payload Too Large
 
-    public static Result PayloadTooLarge() => new(StatusCodes.Status413PayloadTooLarge);
+    public static Result PayloadTooLarge() =>
+        new(StatusCodes.Status413PayloadTooLarge);
 
     // 414 URI Too Large
 
-    public static Result UriTooLong() => new(StatusCodes.Status414UriTooLong);
+    public static Result UriTooLong() =>
+        new(StatusCodes.Status414UriTooLong);
 
     // 415 Unsupported Media Type
 
-    public static Result UnsupportedMediaType() => new(StatusCodes.Status415UnsupportedMediaType);
+    public static Result UnsupportedMediaType() =>
+        new(StatusCodes.Status415UnsupportedMediaType);
 
     // 416 Range Not Satisfiable
 
     public static Result RangeNotSatisfiable(StringValues contentRange) =>
         new Result(StatusCodes.Status416RangeNotSatisfiable)
-            .WithFormatting(context => context.HttpContext.Response.Headers.ContentRange = contentRange);
+            .AddFormatter(context => context.HttpContext.Response.Headers.ContentRange = contentRange);
 
     // 417 Expectation Failed
 
-    public static Result ExpectationFailed() => new(StatusCodes.Status417ExpectationFailed);
+    public static Result ExpectationFailed() =>
+        new(StatusCodes.Status417ExpectationFailed);
 
     // 418 I'm a Teapot
 
-    public static Result ImATeapot() => new(StatusCodes.Status418ImATeapot);
+    public static Result ImATeapot() =>
+        new(StatusCodes.Status418ImATeapot);
 
     // 428 Precondition Required
 
-    public static Result PreconditionRequired() => new(StatusCodes.Status428PreconditionRequired);
+    public static Result PreconditionRequired() =>
+        new(StatusCodes.Status428PreconditionRequired);
 
     // 431 Request Header Fields Too Large
 
-    public static Result RequestHeaderFieldsTooLarge() => new(StatusCodes.Status431RequestHeaderFieldsTooLarge);
+    public static Result RequestHeaderFieldsTooLarge() =>
+        new(StatusCodes.Status431RequestHeaderFieldsTooLarge);
 
     // 451 Unavailable for Legal Reasons
 
-    public static Result UnavailableForLegalReasons() => new(StatusCodes.Status451UnavailableForLegalReasons);
+    public static Result UnavailableForLegalReasons() =>
+        new(StatusCodes.Status451UnavailableForLegalReasons);
 
     // 500 Internal Server Error
 
-    public static Result InternalServerError() => new(StatusCodes.Status500InternalServerError);
+    public static Result InternalServerError() =>
+        new(StatusCodes.Status500InternalServerError);
 
     // 501 Not Implemented
 
     public static Result NotImplemented(StringValues? retryAfter) =>
         new Result(StatusCodes.Status501NotImplemented)
-            .WithFormatting(
+            .AddFormatter(
                 context =>
                 {
                     if (retryAfter is not null)
@@ -447,31 +468,38 @@ public class Result : IResult
 
     // 502 Bad Gateway
 
-    public static Result BadGateway() => new(StatusCodes.Status502BadGateway);
+    public static Result BadGateway() =>
+        new(StatusCodes.Status502BadGateway);
 
     // 503 Service Unavailable
 
-    public static Result ServiceUnavailable() => new(StatusCodes.Status503ServiceUnavailable);
+    public static Result ServiceUnavailable() =>
+        new(StatusCodes.Status503ServiceUnavailable);
 
     // 504 Gateway Timeout
 
-    public static Result GatewayTimeout() => new(StatusCodes.Status504GatewayTimeout);
+    public static Result GatewayTimeout() =>
+        new(StatusCodes.Status504GatewayTimeout);
 
     // 505 HTTP Version Not Supported
 
-    public static Result HttpVersionNotSupported() => new(StatusCodes.Status505HttpVersionNotsupported);
+    public static Result HttpVersionNotSupported() =>
+        new(StatusCodes.Status505HttpVersionNotsupported);
 
     // 506 Variant Also Negotiates
 
-    public static Result VariantAlsoNegotiates() => new(StatusCodes.Status506VariantAlsoNegotiates);
+    public static Result VariantAlsoNegotiates() =>
+        new(StatusCodes.Status506VariantAlsoNegotiates);
 
     // 510 Not Extended
 
-    public static Result NotExtended() => new(StatusCodes.Status510NotExtended);
+    public static Result NotExtended() =>
+        new(StatusCodes.Status510NotExtended);
 
     // 511 Network Authentication Required
 
-    public static Result NetworkAuthenticationRequired() => new(StatusCodes.Status511NetworkAuthenticationRequired);
+    public static Result NetworkAuthenticationRequired() =>
+        new(StatusCodes.Status511NetworkAuthenticationRequired);
 
     protected virtual Task ExecuteResultInternalAsync(ActionContext context) =>
         Task.CompletedTask;
